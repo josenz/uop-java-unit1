536f7d1ddb0dec5e87f8e584e351773b80d23822
josenz <jose.santos.soares@hotmail.com>
2020-11-16 18:39:49 +1300

    Removed Hello files

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@
 	public static void main(String[] args) {
  	    
     	    MountainBike myBike = new MountainBike(1, 2, 3, 4);
-	    //  myBike.setHeight(22);
+	     myBike.setHeight(22);
 	    int myHeight = myBike.getHeight();
  	    System.out.println("The height is " + myHeight);
 	    myBike.printMessage();    	    
